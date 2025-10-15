9060573bf63a6e53ed52cb21333adfe17d8d955c
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-10-15 20:43:19 +0600

    netlify added

--- streakmaster frontend/vite.config.js
+++ streakmaster frontend/vite.config.js
@@ -6,10 +6,19 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+//   base: process.env.VITE_BASE_PATH || "/StreakMaster-Frontend-",
+// });
+
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/StreakMaster-Frontend-",
+  plugins: [react(), tailwindcss()],
+  base: "./",
 });
